07cf76962e286730c79477975a014866dacb749e
Jeisson Sachica <jeisson.sachica@gmail.com>
2021-02-01 17:37:12 -0500

    Adds commented pathEffect

--- app/src/main/java/com/example/patheffectrepro/DashedCircle.kt
+++ app/src/main/java/com/example/patheffectrepro/DashedCircle.kt
@@ -39,7 +39,7 @@ fun DashedCircle(
                 strokeColor,
                 style = Stroke(
                     width = strokeWidth,
-                    pathEffect = null
+//                    pathEffect = pathEffect
                 )
             )
             drawLine(
